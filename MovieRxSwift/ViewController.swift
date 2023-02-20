0a37908df70346f6d7a56b615fd0a74e635e5e87
VuCuongHD96 <xuancuonghd1102@gmail.com>
2023-02-20 21:54:05 +0700

    Task [1.2]: implement lib

--- MovieRxSwift/ViewController.swift
+++ MovieRxSwift/ViewController.swift
@@ -6,12 +6,19 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
+import NSObject_Rx
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        Observable.just(1)
+            .subscribe { number in
+                print("--- debug ---- ", number)
+            }
+            .disposed(by: rx.disposeBag)
     }
 
 
